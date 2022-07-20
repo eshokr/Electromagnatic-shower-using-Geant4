545494a46ad9263d5715e2600fc9e97e590e9806
eshokr <43872520+eshokr@users.noreply.github.com>
2022-07-20 01:30:55 -0700

    Update physics.cc

--- B4b/src/physics.cc
+++ B4b/src/physics.cc
@@ -3,6 +3,10 @@
 MyPhysicsList::MyPhysicsList()
 {
     RegisterPhysics (new G4EmStandardPhysics());
+    //RegisterPhysics (new G4EmStandardPhysics_option1());
+    //RegisterPhysics (new G4EmStandardPhysics_option2());
+    //RegisterPhysics (new G4EmStandardPhysics_option3());
+
     //RegisterPhysics (new G4OpticalPhysics());
   //  RegisterPhysics(new G4DecayPhysics());
    // RegisterPhysics(new G4RadioactiveDecayPhysics());
